658ab740ae6750522485fc1e984596007c2e492e
Ilia Vatahov <ivvatahov@gmail.com>
2015-07-21 10:52:30 +0300

    initial commit

---
+++ data/cache/classes/cache.php
@@ -0,0 +1,31 @@
+<?php 
+/** 
+ * AutoloadManager Script
+ * 
+ * @authors      Al-Fallouji Bashar & Charron Pierrick
+ * 
+ * @description This file was automatically generated at 2015-02-19 [12:29:16]
+ * 
+ */ 
+return array (
+  'autoloadmanager' => 'C:\\xampp\\htdocs\\www\\application\\autoloadManager.php',
+  'homecontroller' => 'C:\\xampp\\htdocs\\www\\application\\controller\\homeController.php',
+  'indexcontroller' => 'C:\\xampp\\htdocs\\www\\application\\controller\\indexController.php',
+  'logincontroller' => 'C:\\xampp\\htdocs\\www\\application\\controller\\loginController.php',
+  'logoutcontroller' => 'C:\\xampp\\htdocs\\www\\application\\controller\\logoutController.php',
+  'registercontroller' => 'C:\\xampp\\htdocs\\www\\application\\controller\\registerController.php',
+  'uploadcontroller' => 'C:\\xampp\\htdocs\\www\\application\\controller\\uploadController.php',
+  'basecontroller' => 'C:\\xampp\\htdocs\\www\\application\\core\\baseController.php',
+  'registry' => 'C:\\xampp\\htdocs\\www\\application\\core\\registry.php',
+  'router' => 'C:\\xampp\\htdocs\\www\\application\\core\\router.php',
+  'template' => 'C:\\xampp\\htdocs\\www\\application\\core\\template.php',
+  'desk' => 'C:\\xampp\\htdocs\\www\\application\\model\\dao\\desk.php',
+  'document' => 'C:\\xampp\\htdocs\\www\\application\\model\\dao\\document.php',
+  'user' => 'C:\\xampp\\htdocs\\www\\application\\model\\dao\\user.php',
+  'deskdto' => 'C:\\xampp\\htdocs\\www\\application\\model\\dto\\deskDto.php',
+  'documentdto' => 'C:\\xampp\\htdocs\\www\\application\\model\\dto\\documentDto.php',
+  'userdto' => 'C:\\xampp\\htdocs\\www\\application\\model\\dto\\userDto.php',
+  'authenticationrepository' => 'C:\\xampp\\htdocs\\www\\application\\repository\\authenticationRepository.php',
+  'userrepository' => 'C:\\xampp\\htdocs\\www\\application\\repository\\userRepository.php',
+  'authentication' => 'C:\\xampp\\htdocs\\www\\includes\\security\\authentication.php',
+);
